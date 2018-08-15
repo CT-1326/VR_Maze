4490d20fb260667efa460e1e9abad6e6aa0adb3a
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2018-08-15 23:56:17 +0900

    Initial commit

---
+++ Assets/follow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour {
+
+    public Transform player;
+    public float dist = 1f;
+    public float height = 300.0f;
+    public float smoothRotate = 5.0f;
+    private Transform tr;
+
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        float currYAngle = Mathf.LerpAngle(tr.eulerAngles.y, player.eulerAngles.y, smoothRotate * Time.deltaTime);
+
+        Quaternion rot = Quaternion.Euler(0, currYAngle, 0);
+        
+        tr.position = player.position - (rot * Vector3.forward * dist);
+
+
+        tr.LookAt(player);
+	}
+}
---
+++ Assets/go.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class go : MonoBehaviour {
+
+    float sp = 2f;
+    float rotsp = 1.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.transform.localScale = new Vector3(0.3f, 1f, 0.3f);
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //transform.Translate(Vector3.back * sp * Time.deltaTime);
+            this.transform.localScale = new Vector3(0.3f,0.3f,0.3f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * sp * Time.deltaTime);
+            this.transform.Rotate(0.0f, -90.0f * Time.deltaTime * 2, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * sp * Time.deltaTime);
+            this.transform.Rotate(0.0f, 90.0f * Time.deltaTime * 2, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * sp * Time.deltaTime);
+        }
+    }
+}
