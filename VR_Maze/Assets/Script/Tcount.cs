d216d1457d4ba7d4ac066904e3c70020512e5785
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2018-11-09 01:09:17 +0900

    FIX
    add timer

---
+++ VR_Maze/Assets/Script/Tcount.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tcount : MonoBehaviour {
+
+    public Text timeLabel;
+    public float timeCount;
+
+	// Use this for initialization
+	void Start () {
+        timeCount = 15;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timeCount > 0) timeCount -= Time.deltaTime;
+        timeLabel.text = string.Format("{0:N0}", timeCount);
+		
+	}
+}
