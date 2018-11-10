8c0aa069a67eb529dab395f2b6f1f7073ed30875
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2018-11-11 03:53:27 +0900

    FIX
    add exit window

---
+++ VR_Maze/Assets/Script/Re.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Re : MonoBehaviour {
+
+
+    public void rebu()
+    {
+        SceneManager.LoadScene("miro");
+       
+        
+    }
+}
--- VR_Maze/Assets/Script/Tcount.cs
+++ VR_Maze/Assets/Script/Tcount.cs
@@ -7,9 +7,11 @@ public class Tcount : MonoBehaviour {
 
     public Text timeLabel;
     public float timeCount;
+    public Canvas cv;
 
 	// Use this for initialization
 	void Start () {
+        Time.timeScale = 1f;
         timeCount = 15;
 		
 	}
@@ -17,8 +19,18 @@ public class Tcount : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (timeCount > 0) timeCount -= Time.deltaTime;
+        if (timeCount > 0)
+            timeCount -= Time.deltaTime;
+        else if (timeCount < 0)
+            exit();
         timeLabel.text = string.Format("{0:N0}", timeCount);
-		
-	}
+
+    }
+
+    void exit()
+    {
+        Time.timeScale = 0;
+        cv.gameObject.SetActive(true);
+        
+    }
 }
